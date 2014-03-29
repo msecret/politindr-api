33b6a26238df35a221241cf261f6fc7700ea666b
Marco Secreto <msegreto@miceover.com>
2014-03-29 12:50:58 -0700

    Fixed port number

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ def hello():
     return 'politindr init!\n'
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="8080", debug=True)
+    app.run(host="127.0.0.1", port="8080", debug=True)
